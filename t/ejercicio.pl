8f2e7c67552b24105bf0b2f00df7ddf6b063cca5
legajo8716 <nelgonzalez88@gmail.com>
2021-06-05 23:57:44 -0300

    continuando

--- t/ejercicio.pl
+++ t/ejercicio.pl
@@ -1,4 +1,4 @@
-package ejercicio 2;
+package ejercicio;
 use strict;
 use warnings FATAL => 'all';
 
@@ -18,7 +18,7 @@ sub deDecABin{
     my$cantDigitos=length($nAConvetir);
     my$numeroAux=$nAConvetir;
     my @listaNConvertido=();
-   while($numeroAux>2){
+   while($numeroAux>=2){
        $numeroAux=$numeroAux/2;
         if ($numeroAux-int($numeroAux)>0) { ##encapsular comportamiento en una funcion
           push(@listaNConvertido,1)
@@ -28,7 +28,13 @@ sub deDecABin{
         }
         $numeroAux =int($numeroAux);
     }
-    print(@listaNConvertido)
+    if($numeroAux<1){
+        push(@listaNConvertido,0)
+    }
+    else{
+        push(@listaNConvertido,1)
+    }
+    return(reverse(@listaNConvertido))
 }
 
 
@@ -41,26 +47,117 @@ sub convertir {
     return $numero;
 }
 
-sub pruebaCadena{
-    my($cadena)=shift @_;
-    my$cadenaProcesada=$cadena;
 
-    return $cadenaProcesada;
+sub baseDatosHexa{
+    my($base1)=shift @_;
+    my($numero)=shift @_;
+    my($base2)=shift @_;
+    my%hexaABin=(0=>0000,
+                 1=>0001,
+                 2=>0010,
+                 3=>0011,
+                 4=>0100,
+                 5=>0101,
+                 6=>0110,
+                 7=>0111,
+                 8=>1000,
+                 9=>1001,
+                 "A"=>1010,
+                 "B"=>1011,
+                 "C"=>1100,
+                 "D"=>1101,
+                 "E"=>1110,
+                 "F"=>1111);
+    my%binAHexa=(0000=>0,
+                 0001=>1,
+                 0010=>2,
+                 0011=>3,
+                 0100=>4,
+                 0101=>5,
+                 0110=>6,
+                 0111=>7,
+                 1000=>8,
+                 1001=>9,
+                 1010=>"A",
+                 1011=>"B",
+                 1100=>"C",
+                 1101=>"D",
+                 1110=>"E",
+                 1111=>"F");
+    my%hexaADec=(0=>0,
+                 1=>1,
+                 2=>2,
+                 3=>3,
+                 4=>4,
+                 5=>5,
+                 6=>6,
+                 7=>7,
+                 8=>8,
+                 9=>9,
+                 "A"=>10,
+                 "B"=>11,
+                 "C"=>12,
+                 "D"=>13,
+                 "E"=>14,
+                 "F"=>15);
+    my %decAHexa=(0=>0,
+                 1=>1,
+                 2=>2,
+                 3=>3,
+                 4=>4,
+                 5=>5,
+                 6=>6,
+                 7=>7,
+                 8=>8,
+                 9=>9,
+                 10=>"A",
+                 11=>"B",
+                 12=>"C",
+                 13=>"D",
+                 14=>"E",
+                 15=>"F");
+    my$resultado=-1;
+    if($base1==16 and $base2==2){
+     $resultado=%hexaABin->$numero;
+    }
+    elsif($base1==16 and $base2==10){
+        $resultado=%hexaADec.$numero;
+    }
+    elsif($base1==2 and $base2==16){
+        $resultado=%binAHexa.$numero;
+    }
+    elsif($base1==10 and $base2==16){
+        $resultado=%decAHexa.$numero;
+    }
+    return $resultado;
 }
 
-sub pruebaIteraciones{
-    my($cadena)=shift@_;
-    for(1..length($cadena)){
-        print("hola ");
+sub deHexaA() {
+    my ($base) = shift @_;
+    my ($numero) = shift @_;
+    my @resultado = {};
+    my $numeroAux = $numero;
+    if ($base == 10) {
+        for (0 .. length($numero)) {
+            push(@resultado, baseDatosHexa(16, chop $numeroAux, 10))
+        }
+        if ($base == 2) {
+            for (0 .. length($numero)) {
+                push(@resultado, baseDatosHexa(16, chop $numeroAux, 2))
+            }
+        }
+        return @resultado;
     }
 
 }
 
-my @numero=();
-push(@numero,2);
-push(@numero,3);
 
-deDecABin(1);
+my $num=10;
+my $result=baseDatosHexa(10,"B",16);
+print ($result);
+
+
+
 
 
 
