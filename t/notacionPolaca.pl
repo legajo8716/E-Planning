f7ac3f2e56756a716b5b5b3eb8181a360bf73363
legajo8716 <nelgonzalez88@gmail.com>
2021-06-08 18:24:57 -0300

    terminando notacion polaca inversa

--- t/notacionPolaca.pl
+++ t/notacionPolaca.pl
@@ -1,25 +1,41 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
-
- sub extraerUltimoTermino{
-
- }
- sub sinElUltimoTermino{
+sub sinElUltimoTermino{
      my($cuentaEnNotPol)=shift @_;
      my $cuentaAux=$cuentaEnNotPol;
      my $resultado="";
 
      my $ultimoDigito=chop($cuentaAux);
-     while(ord($ultimoDigito)!=ord(" ")){
-         $resultado=$ultimoDigito.$resultado;
-         $ultimoDigito=chop($cuentaAux);
-     }
+
+         while (ord($ultimoDigito) != ord(" ")&&length($cuentaAux)!=0) {
+             $ultimoDigito = chop($cuentaAux);
+         }
+
+     return $cuentaAux;
+ }
+ sub extraerUltimoTermino{
+
+     my($cuentaEnNotPol)=shift @_;
+     my $cuentaAux=$cuentaEnNotPol;
+     my $resultado="";
+
+     my $ultimoDigito=chop($cuentaAux);
+
+         while (ord($ultimoDigito) != ord(" ")&&length($cuentaAux)!=0) {
+             $resultado = $ultimoDigito . $resultado;
+             $ultimoDigito = chop($cuentaAux);
+         }
+        if(length($cuentaAux)==0){
+            $resultado = $ultimoDigito . $resultado;
+        }
+
      return $resultado;
  }
 sub esUltimoTermino{
     my($cuentaEnNotPol)=shift @_;
     my $longitudCadena=length(sinElUltimoTermino($cuentaEnNotPol));
+
     return($longitudCadena==0);
 }
 sub realizarOperacion{
@@ -28,22 +44,70 @@ sub realizarOperacion{
     my($operacion)=shift @_;
     my $resultado=0;
     if(ord($operacion)==ord("x")){
-        $resultado=$term1*$term2;
+        $resultado=$term2*$term1;
     }
     if(ord($operacion)==ord("/")){
-        $resultado=$term1/$term2;
+        $resultado=$term2/$term1;
 
     }
     if(ord($operacion)==ord("+")){
-        $resultado=$term1+$term2;
+
+
+        $resultado=$term2+$term1;
 
     }
     if(ord($operacion)==ord("-")){
-        $resultado=$term1-$term2;
+        $resultado=$term2-$term1;
 
     }
     return($resultado);
 }
+sub esNumero{
+    my ($digito) = shift @_;
+    return (ord($digito)>47 and ord($digito)<58)
+
+}
+sub esOperando{
+    my ($digito) = shift @_;
+    my $resultado=ord($digito)==ord('+')||ord($digito)==ord('-')||ord($digito)==ord('x')||ord($digito)==ord('/');
+    return($resultado);
+
+}
+sub capturarPrimeraCuenta{
+
+    my ($cuentaEnNotPol) = shift @_;
+
+
+    my $comienzoCuenta=$cuentaEnNotPol;
+    my $cuentaProcesada='';
+    my $finalCuenta='';
+
+    my ($operando)=extraerUltimoTermino($comienzoCuenta);
+
+    $comienzoCuenta=sinElUltimoTermino($comienzoCuenta);
+
+    my ($termino1)=extraerUltimoTermino($comienzoCuenta);
+    $comienzoCuenta=sinElUltimoTermino($comienzoCuenta);
+    my ($termino2)=extraerUltimoTermino($comienzoCuenta);
+    $comienzoCuenta=sinElUltimoTermino($comienzoCuenta);
+
+
+    while(!(esOperando($operando)&& esNumero($termino1)&& esNumero($termino2))){
+
+        $finalCuenta= " ".$operando .$finalCuenta;
+        $operando=$termino1;
+        $termino1=$termino2;
+        $termino2=extraerUltimoTermino($comienzoCuenta);
+        $comienzoCuenta=sinElUltimoTermino($comienzoCuenta);
+
+    }
+       $cuentaProcesada=realizarOperacion($termino1,$termino2,$operando);
+        return $comienzoCuenta .$cuentaProcesada . $finalCuenta;
+
+}
+
+
+
  sub efectuarOperacion {
      my ($cuentaEnNotPol) = shift @_;
      my ($cuentaParaProcesar) = $cuentaEnNotPol;
@@ -51,16 +115,14 @@ sub realizarOperacion{
      my $ultimoTermino = '';
      my $operacion = '';
 
-     if (esUltimoTermino($cuentaParaProcesar)) {
-         return extraerUltimoTermino($cuentaParaProcesar);
+     while(!esUltimoTermino($cuentaParaProcesar)) {
+         $cuentaParaProcesar= capturarPrimeraCuenta($cuentaParaProcesar);
+
      }
-     else {
-         $ultimoTermino = extraerUltimoTermino($cuentaParaProcesar);
-         $cuentaParaProcesar = sinElUltimoTermino($cuentaParaProcesar);
-         $operacion = sinElUltimoTermino($cuentaParaProcesar);
-         $cuentaParaProcesar = sinElUltimoTermino($cuentaParaProcesar);
-         $resultado = realizarOperacion(efectuarOperacion($cuentaParaProcesar), $ultimoTermino, $operacion);
-     }
+     $resultado=$cuentaParaProcesar;
+     return($resultado);
 
  }
-print(efectuarOperacion("23 4 +"));
+my$bool=(esOperando('+')&& esNumero(1)&& esNumero(2));
+my $tamao=efectuarOperacion("23 5 - 53 +");
+print($tamao);
